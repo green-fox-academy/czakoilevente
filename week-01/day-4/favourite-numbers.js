af6e6fec960af58c863a4083f0bfdbc4902d3ae4
Czakoi Levente <czakoilevente@gmail.com>
2019-01-17 15:54:41 +0100

    fav nums done

---
+++ week-01/day-4/favourite-numbers.js
@@ -0,0 +1,6 @@
+'use strict';
+var favoriteNumber = 8;
+// Store your favorite number in a variable (as a number)
+var fn = 8;
+// And print it like this: 'My favorite number is: 8'
+console.log("My favourtie number is: ", fn);

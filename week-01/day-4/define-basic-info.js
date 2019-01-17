c6cc0f15adc46930db6a8933b23950f1c01002c1
Czakoi Levente <czakoilevente@gmail.com>
2019-01-17 16:21:45 +0100

    basic info done

---
+++ week-01/day-4/define-basic-info.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+var myFeet = 45; //shoesize
+// Your name as a string
+var myName = "Czakói Levente";
+// Your age as an integer
+var myAge = 33;
+// Your height in meters as a float
+var myHeight = 1.82;
+// Whether you are married or not as a boolean
+var myStatus = false;
---
+++ week-01/day-4/define-basic-info.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+let myFeet = 45 //shoesize
+
+// Your name as a string
+let myName:string = "Czakói Levente"
+
+// Your age as an integer
+let myAge: number = 33;
+
+// Your height in meters as a float
+let myHeight = 1.82;
+
+
+// Whether you are married or not as a boolean
+let myStatus:boolean = false;
+

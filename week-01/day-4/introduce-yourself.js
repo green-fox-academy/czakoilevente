fc8f2546486bea5978486ccd81b47c0ba7913794
Czakoi Levente <czakoilevente@gmail.com>
2019-01-17 15:08:12 +0100

    introduction

---
+++ week-01/day-4/introduce-yourself.js
@@ -0,0 +1,5 @@
+'use strict';
+console.log("My name is: Levente Czakói");
+console.log("My age is: 33");
+console.log("My height is: 1.82m");
+console.log("My haircolor is brow"); // I know its not mandatory, just thought u wanna know :P

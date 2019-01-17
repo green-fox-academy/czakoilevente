1d55a8089e6034cc76e82a17856b1ddd918ccb2e
Czakoi Levente <czakoilevente@gmail.com>
2019-01-17 16:10:50 +0100

    body mass index finished

---
+++ week-01/day-4/bmi.js
@@ -0,0 +1,7 @@
+'use strict';
+var massInKg = 81.2;
+var heightInM = 1.78;
+// Print the Body mass index (BMI) based on these values
+//BMI = mass / height**2
+var BMI = massInKg / Math.pow(heightInM, 2);
+console.log(BMI);
---
+++ week-01/day-4/bmi.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+// Print the Body mass index (BMI) based on these values
+
+//BMI = mass / height**2
+
+var BMI: number = massInKg/heightInM**2
+
+console.log(BMI)

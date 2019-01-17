de44681a470662b11fbea32a6010c96883468fd3
Czakoi Levente <czakoilevente@gmail.com>
2019-01-17 15:44:09 +0100

    coding hours done

---
+++ week-01/day-4/coding-hours.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+
+// An average Green Fox attendee codes 6 hours daily
+var c = 6
+// The semester is 17 weeks long
+var s = 17//
+
+
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+var b = 5 //workdays r 5 on the week
+var n = c*s*b 
+console.log(n)
+
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
+var t = 52 
+
+var r = t*s //total working hours in 17 weeks
+
+var e = n/r*100 //percentage result
+
+console.log(e)

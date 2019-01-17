870a596c167c3498b2a8837ba943b137ac1a459d
Czakoi Levente <czakoilevente@gmail.com>
2019-01-17 16:05:11 +0100

    swap doneú

---
+++ week-01/day-4/swap.js
@@ -0,0 +1,9 @@
+'use strict';
+// Swap the values of these variables
+var a = 123;
+var b = 526;
+console.log(a);
+console.log(b);
+var c = b;
+b = a;
+a = c;
---
+++ week-01/day-4/swap.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+// Swap the values of these variables
+let a: number = 123;
+let b: number = 526;
+
+console.log(a);
+console.log(b);
+
+var c: number = b;
+
+b = a;
+
+a = c;
+

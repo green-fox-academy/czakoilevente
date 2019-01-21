0fffd1c07e6e73a23434d8fea0915909ed62cdcb
Czakoi Levente <czakoilevente@gmail.com>
2019-01-21 15:12:39 +0100

    doubling done

---
+++ week2/d1/greet.ts
@@ -0,0 +1,15 @@
+// -  Create variable named `name` and assign the value `Greenfox` to it
+// -  Create a function called `greet` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Greenfox`
+//     -  Prepare for the special case when no parameters are given
+// -  Greet `name`
+
+let namee = "Greenfox";
+
+
+function greet(x: string) {
+
+    return "Greetings, dear " + x;
+}
+
+console.log(greet(namee));

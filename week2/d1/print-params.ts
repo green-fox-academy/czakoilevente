b6ad7ea446be566954decd835981b589010b24fd
Czakoi Levente <czakoilevente@gmail.com>
2019-01-21 17:36:55 +0100

    monday done

---
+++ week2/d1/print-params.ts
@@ -0,0 +1,11 @@
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+function printParams (a, b, c, d) {
+    return a; b; c; d;
+}
+console.log(printParams(2,3,4,5))
+
+//-----------------------------------------
+

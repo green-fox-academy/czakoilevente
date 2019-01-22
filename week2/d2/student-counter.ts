3191c53765f2ddd91acf115aed71159bc04175b2
Czakoi Levente <czakoilevente@gmail.com>
2019-01-22 17:25:36 +0100

    some stuff

--- week2/d2/student-counter.ts
+++ week2/d2/student-counter.ts
@@ -14,5 +14,14 @@ const students: any[] = [
 
 // create a function that takes a list of students and logs:
 // - Sum of the age of people who have lass than 5 candies
-© 2019 GitHub, Inc.
+// © 2019 GitHub, Inc.
+
+// function countNames(x: any)
+
+for ();
+
+// let keyArr = students.keys().split(",").length
+
+// for -> összeszámolni hányszor fut let a loop
+
 

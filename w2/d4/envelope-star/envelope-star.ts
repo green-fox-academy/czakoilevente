88eb42d48d78e082524b28b3426ed90f80a3db65
Czakoi Levente <czakoilevente@gmail.com>
2019-01-24 16:26:55 +0100

    envelope 1st quarter with variable

--- w2/d4/envelope-star/envelope-star.ts
+++ w2/d4/envelope-star/envelope-star.ts
@@ -3,51 +3,45 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-//--------------------------------------------------------------------------------------
+//--------------- dont touch over this line
 
 let tr = 50;  //<- mérethez alakító szorzó
-let t = 10;      // <- vonalak sűrűsége akar lenni
+//let t = 10;      // <- vonalak sűrűsége akar lenni
 
-
-let x1 = 1;
+/* let x1 = 1;
 let y1 = 0;   //<- állandó
 
 let x2 = 10;  //<- állandó
-let y2 = 1;
+let y2 = 1; */
+
+let x = 1;
 
 
-
-function drawLine(x1, y2) {
-ctx.strokeStyle = 'purple';
-ctx.beginPath();
-ctx.moveTo(tr * x1, tr * 0);
-ctx.lineTo(tr * 10, tr * y2);
-ctx.stroke();
+function drawLine(x) {
+    ctx.strokeStyle = 'purple';
+    ctx.beginPath();
+    ctx.moveTo(tr * x, tr * 0 * x);
+    ctx.lineTo(tr * 10 , tr * x );
+    ctx.stroke();
 }
 
 for (let k = 1; k < 11; k++) {
-    drawLine(k, k)
+    drawLine(k)
 };
-
-
 
 // line1 x1      =   line1 y2+10
 // line2 x1+10   =   line2 y2+10+10
-
 // line1 y1      =   line1 x2 
 
 
-
-
-
-function drawLine2(x2, y1) {
+/* function drawLine2(x2, y1) {
     ctx.strokeStyle = 'green';
     ctx.beginPath();
     ctx.moveTo(tr * 0, tr * y1);
     ctx.lineTo(tr * x2, tr * 10);
     ctx.stroke();
-    }
+}
 
-    for (let k = 1; k < t+1; k++) {
-        drawLine2(k, k)
-    };
+for (let k = 1; k < t + 1; k++) {
+    drawLine2(k, k)
+}; */

d8e6567ef5965622a45f26c7f6690ba4433cb317
Czakoi Levente <czakoilevente@gmail.com>
2019-01-24 13:22:10 +0100

    on the road

--- w2/d4/line-play/line-play.ts
+++ w2/d4/line-play/line-play.ts
@@ -5,19 +5,30 @@ const ctx = canvas.getContext('2d');
 
 //--------------------------------------------------------------------------------------
 
-let tr = 200;
+let tr = 100;
 
-let x1 = 3;
-let y1 = 0;
+let x1 = 1;
+let y1 = 0;   //<- állandó
 
-let x2 = 16;
+let x2 = 10;  //<- állandó
 let y2 = 1;
 
+for (let k = 1; k < 11; k++) {
+
+}
 
 ctx.strokeStyle = 'purple';
 ctx.beginPath();
-ctx.moveTo(x1, y1);
-ctx.lineTo(x2, y2);
+ctx.moveTo(tr * x1, tr * 0);
+ctx.lineTo(tr * 10, tr * y2);
 ctx.stroke();
+
+
+
+
+// line1 x1      =   line1 y2+10
+// line2 x1+10   =   line2 y2+10+10
+
+// line1 y1      =   line1 x2 
 
 

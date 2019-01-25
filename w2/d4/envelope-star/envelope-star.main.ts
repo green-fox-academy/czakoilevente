8a3466222cd29a5f640c865dcd533012e64f406a
Czakoi Levente <czakoilevente@gmail.com>
2019-01-25 14:41:20 +0100

    w2 friday :)

--- w2/d4/envelope-star/envelope-star.main.ts
+++ w2/d4/envelope-star/envelope-star.main.ts
@@ -3,18 +3,18 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-let NumberOfLines: number = 100;
+let NumberOfLines: number = 50;
 let Width: number = 1000;
 let Height: number = 1000;
 
 function drawLine3(x1: number, y1:number, x2: number, y2: number) {
-  ctx.strokeStyle = 'purple';
+  ctx.strokeStyle = 'teal';
   ctx.beginPath();
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.stroke();
 }
-for (let k: number = 0; k < NumberOfLines + 1; k++) {
+for (let k: number = 1; k < NumberOfLines ; k++) {
 
 //Coordinates: X1                      Y1                                   X2                                    Y2
   drawLine3( Width/2/NumberOfLines*k , Height/2                           , Width/2                             , Height/2/NumberOfLines*k + Height/2 );
---
+++ w2/d4/envelope-star/readme.txt
@@ -0,0 +1,17 @@
+
+Pros	* 1 function
+
+	* 1 loop
+
+	* 3 input
+
+	* adaptable to:	- canvas size
+			- item size (resizeable)
+			- shape
+			- filling => u can set the numbers of lines
+
+	* length: 24 lines
+
+	* easy to separate the blocks - individual blocks
+
+	* easy to upscale

2483fa3e68dccfc526e8b8b86276265db4050b76
Czakoi Levente <czakoilevente@gmail.com>
2019-01-29 21:37:42 +0100

    homework for wednesday

---
+++ w3/d3/HW/homework.ts
@@ -0,0 +1,12 @@
+'use strict'
+let trees: { name: string, age: number, leaf_colour: string, sex: string}[] =
+[
+   {"name": "oak", "age": 30, "leaf_colour": "green", "sex": "never" },
+   {"name": "apple", "age": 5, "leaf_colour": "green", "sex": "nope" },
+   {"name": "pine", "age": 10, "leaf_colour": "green", "sex": "forget it" },
+   {"name": "pear", "age": 3, "leaf_colour": "green", "sex": "umm, no" },
+   {"name": "cherry", "age": 1, "leaf_colour": "green", "sex": "ladies first"}
+];
+
+console.log(trees)
+

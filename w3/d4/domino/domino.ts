c3c0d29276a3f15567678dab0508e353909ba0a5
Czakoi Levente <czakoilevente@gmail.com>
2019-02-01 19:16:34 +0100

    class mass - domino

--- w3/d4/domino/domino.ts
+++ w3/d4/domino/domino.ts
@@ -1,8 +1,13 @@
 class Domino {
   values: number[];
+
   constructor(valueA: number, valueB: number) {
-      this.values = [valueA, valueB];
+    this.values = [valueA, valueB];
   }
 }
 
 export {Domino};
+
+
+/* let domino1 = new Domino(2,3);
+let domino2 = new Domino(3,4); */
--- w3/d4/domino/dominos.ts
+++ w3/d4/domino/dominos.ts
@@ -1,15 +1,29 @@
-import {Domino} from "./domino";
+//initializeDominoes(dominoes);
+
+/** You have the list of Dominoes */
+/** Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides */
+/** eg: [2, 4], [4, 3], [3, 5] ... */
+
+import { Domino } from "./domino";
+
+/* function shuffleDominos (dominoes: Domino[]){
+    dominoes
+}; */
 
 function initializeDominoes(): Domino[] {
     let dominoes = [];
+
     dominoes.push(new Domino(5, 2));
     dominoes.push(new Domino(4, 6));
     dominoes.push(new Domino(1, 5));
     dominoes.push(new Domino(6, 7));
-    dominoes.push(new Domino(2 ,4));
+    dominoes.push(new Domino(2, 4));
     dominoes.push(new Domino(7, 1));
+//shuffleDominos(dominoes)
     return dominoes;
 }
+
+
 
 
 function print(dominoes: Domino[]) {
@@ -19,10 +33,32 @@ function print(dominoes: Domino[]) {
 }
 
 let dominoes = initializeDominoes();
-/** You have the list of Dominoes */
-/** Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides */
-/** eg: [2, 4], [4, 3], [3, 5] ... */
 
-//print(dominoes);
+function dominoRuler(dominoes, i) {
+    let movingDomino = dominoes.splice(i, 1)[0];
+    dominoes.push(movingDomino);
+    return dominoes;
+};
 
-console.log(dominoes)
+for (let i = 0; i < dominoes.length - 1; i++) {
+    while (dominoes[i].values[1] !== dominoes[i+1].values[0]) {
+        if (dominoes[i].values[1] !== dominoes[i+1].values[0]){
+            console.log('Shuffle failed! Try Harder Stupid. :) ')
+        break
+        };
+
+    dominoRuler(dominoes, i+1);
+
+    };
+    console.log(dominoes);
+};
+
+
+console.log('--------------------------------');
+console.log('console.log: ' + "\n");
+
+console.log(dominoes);
+console.log('--------------------------------');
+console.log('print dominoes: ' + "\n" );
+
+print(dominoes);

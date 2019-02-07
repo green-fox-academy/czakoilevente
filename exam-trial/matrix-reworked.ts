3614d6854d44b39271e9e9b911171d56525a9079
Czakoi Levente <czakoilevente@gmail.com>
2019-02-07 11:15:18 +0100

    matrix finished

--- exam-trial/matrix-reworked.ts
+++ exam-trial/matrix-reworked.ts
@@ -6,30 +6,8 @@ and returns true if the matrix is symmetric
 or false if it is not.
 (Symmetric means it has identical values along its diagonal axis from top-left to bottom-right,
 as in the first example)
-Example 1:
-*/
-let example1: number[][] =[
-  [1, 0, 1],
-  [0, 2, 2],
-  [1, 2, 5]
-];
-/*Output:
 
-true
-Example 2:
-*/
-let example2: number[][] =[
-  [7, 7, 7],
-  [6, 5, 7],
-  [1, 2, 1]
-];
-/*Output:
-
-false
-*/
-
-function isSymmetricc(matrix: number[][]) {
-  /*matrix[0][0] === matrix[0][0];
+/*  matrix[0][0] === matrix[0][0];
     matrix[0][1] === matrix[1][0];
     matrix[0][2] === matrix[2][0];
 
@@ -43,39 +21,36 @@ function isSymmetricc(matrix: number[][]) {
 
   //matrix[i][j] === matrix[i][j]
 
-  console.log(matrix);
+// SOLUTION OF THE TASK STARTS FROM HERE (upper part only for self guide)
+
+let example1: number[][] =[
+  [1, 0, 1],
+  [0, 2, 2],
+  [1, 2, 5]
+];
+
+let example2: number[][] =[
+  [7, 7, 7],
+  [6, 5, 7],
+  [1, 2, 1]
+];
+
+function isSymmetricc(matrix: number[][]) {
+  
+  console.log(matrix); //just to see the content of actual array
+
   let result: boolean = true;
   
     for (let i = 0; i < matrix.length; i++) {
       for (let j = 0; j < matrix.length; j++) {
         if (matrix[i][j] !== matrix[j][i]){
           result = false;
-          /*         console.log('Symmetric!')
-        } else {console.log('Not symmetric!')} */
-      };
+        };
     };
-    return result
   };
+  return result
 };
 
-isSymmetricc(example1);
+console.log(isSymmetricc(example1));
 console.log('-------------------')
-isSymmetricc(example2);
-
-
-/*
- function isSymmetric(matrix: number[][]): void {
-
-  matrix[0][0] === matrix[0][0]
-  matrix[1][0] === matrix[0][1]
-  matrix[2][0] === matrix[0][2]
-
-  matrix[0][1] === matrix[1][0]
-  matrix[1][1] === matrix[1][1]
-  matrix[2][1] === matrix[1][2]
-
-  matrix[0][2] === matrix[2][0]
-  matrix[][] === matrix[][]
-  matrix[][] === matrix[][]
-};
-*/
+console.log(isSymmetricc(example2));

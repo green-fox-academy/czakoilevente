d35976ea05aa44540b5c7ebf97bef99333993b8b
Czakoi Levente <czakoilevente@gmail.com>
2019-02-08 14:00:27 +0100

    myClass test ready + working

--- w4/d4/Sum/myClass.test.ts
+++ w4/d4/Sum/myClass.test.ts
@@ -4,5 +4,12 @@ import { MyClass } from  './myClass';
 
 const test = require('tape');
 
-test();
+test('sum numbers in the array', t => {
+  const operation = new MyClass;
 
+  let actual: number = operation.sum([1,2,3,4,5]);
+  let expected: number = 15;
+
+  t.equal(actual,expected);
+  t.end();
+});

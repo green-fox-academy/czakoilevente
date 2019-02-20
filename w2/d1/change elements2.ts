ee943469470a3dd8fdfc194f427ea89364539eda
Czakoi Levente <czakoilevente@gmail.com>
2019-02-20 18:11:08 +0100

    function type removed

--- w2/d1/change elements2.ts
+++ w2/d1/change elements2.ts
@@ -2,7 +2,7 @@
 
 let nummList: number[] = [1, 2, 3, 8, 5, 6];
 
-function changeElement(oneArray: number[]): number[] {
+function changeElement(oneArray: number[]) {
   return oneArray.map((element: number): number => {
     if (element === 8) {
       return 4;

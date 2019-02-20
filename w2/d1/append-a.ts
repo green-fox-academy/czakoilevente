97e9958f2f2a3cd29dec50872e27f9bb9cf8ff25
Czakoi Levente <czakoilevente@gmail.com>
2019-02-20 17:57:06 +0100

    append-a started

---
+++ w2/d1/append-a.ts
@@ -0,0 +1,13 @@
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+
+let animals: string[] = ["koal", "pand", "zebr"];
+
+function addElement(oneArray: string[]): string[] {
+  
+  for (let i: number = 0; i < oneArray.length; i++) {
+
+    return oneArray.concat();
+  };
+}

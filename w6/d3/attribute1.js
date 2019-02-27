b1492a7643ef9c9069053056c4a22e945b60029e
Czakoi Levente <czakoilevente@gmail.com>
2019-02-27 14:34:26 +0100

    final part missing yet

--- w6/d3/attribute1.js
+++ w6/d3/attribute1.js
@@ -1,3 +1,9 @@
-let imgTag = document.getElementsByName("img");
+let imgTag = document.getElementsByTagName("img");
 
 console.log(imgTag[0].getAttribute("src"));
+
+imgTag[0].setAttribute("src", "https://vigyelhaza.hu/wp-content/uploads/2016/06/%C3%81ron%C3%9Aj-3.jpg");
+
+let aTag = document.getElementsByTagName("a");
+aTag[0].setAttribute("href", "https://www.greenfoxacademy.com/");
+

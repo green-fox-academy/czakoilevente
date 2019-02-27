6969c44a6b2e9967cfb0fa745b258fe4fffc4763
Czakoi Levente <czakoilevente@gmail.com>
2019-02-27 12:34:59 +0100

    html+js added, task done (two results)

---
+++ w6/d3/content-1.js
@@ -0,0 +1,17 @@
+let classContent = document.getElementsByClassName("animals");
+console.log(classContent[0].innerHTML);
+
+let pElemCont = document.getElementsByTagName("p");
+console.log(pElemCont);
+
+/* for (let i = 0; i < pElemCont.length; i++) {
+  // ow: pElemCont[i].innerHTML = classContent[0].innerHTML;
+  // add
+  if (pElemCont[i].innerHTML !== classContent[0].innerHTML) {
+    pElemCont[i].innerHTML += classContent[0].innerHTML;
+  };
+}; */
+
+for (let i = 0; i < pElemCont.length - 1; i++) {
+  pElemCont[i].innerHTML += classContent[0].innerHTML;
+};

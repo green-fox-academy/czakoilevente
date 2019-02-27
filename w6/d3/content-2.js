94fda6362b51bc3148fcea08a9419a9da2ba3d19
Czakoi Levente <czakoilevente@gmail.com>
2019-02-27 13:42:53 +0100

    hmtl+js files added, all set

---
+++ w6/d3/content-2.js
@@ -0,0 +1,25 @@
+let list = ['apple', 'banana', 'cat', 'dog'];
+
+let liElems = document.getElementsByTagName("li");
+
+for (let i = 0; i < liElems.length; i++) {
+  liElems[i].innerHTML = list[i];
+};
+
+/* -> style prperty <-
+let ulTag = document.getElementsByTagName("ul");
+ulTag[0].setAttribute("style", "background-color: limegreen");
+*/
+
+let ulTag = document.getElementsByTagName("ul");  
+//arr.push - ul tags
+ulTag[0].setAttribute("class", "style");          
+//set ul tag attr <ul ATTRIBUT></ul> <ul class="style"></ul>
+
+let styleTag = document.createElement("style");   
+//create empty styleTag: <style></style>
+styleTag.innerHTML = ".style{background-color: limegreen}"; 
+// <style>".style{background-color: limegreen}"</style>
+
+document.getElementsByTagName("body")[0].appendChild(styleTag);
+//puts styleTag into body 

121cce9d5efe58a709e2776c5a1b225be950176d
Czakoi Levente <czakoilevente@gmail.com>
2019-03-05 17:44:09 +0100

    appenda done, added some res.send modifications

--- w7/d1/app.js
+++ w7/d1/app.js
@@ -1,32 +1,32 @@
 'use strict';
 
 const express = require('express');
-const path = require('path')
+const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'assets/index.html'));
 });
 
 app.listen(8080);
 
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-
-app.get(('/doubling'), (req, res) => {
+app.get('/doubling', (req, res) => {
   const inputNumber = Number(req.query.input);
   let myObject = {};
 
   if (req.query.input === undefined) {
     myObject.error = "Please provide an input!"
   } else {
-    myObject.received = req.query.input;
-    myObject.result = (inputNumber * 2).toString();
+    myObject.received = inputNumber;
+    myObject.result = (inputNumber * 2);
   };
 
-  res.send(myObject);
+  res.send(JSON.stringify(myObject));
 });
 
-app.get(('/greeter'), (req, res) => {
+app.get('/greeter', (req, res) => {
   let myObject = {};
 
   if (req.query.name === undefined) {
@@ -35,6 +35,18 @@ app.get(('/greeter'), (req, res) => {
     myObject.welcome_message = `Oh, hi there ${req.query.name}, my dear ${req.query.title}!`;
   };
 
-  res.send(myObject);
+  res.send(JSON.stringify(myObject));
+});
 
-})
+app.get('/appenda/:appendable?', (req, res) => {
+  let myObject = {};
+
+  if (req.params.appendable === undefined) {
+    res.status(404).send('Error! Page not found');
+  } else {
+    myObject.appended = req.params.appendable + "a";
+  };
+
+  res.send(JSON.stringify(myObject));
+});
+

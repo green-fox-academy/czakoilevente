952665bc5984cd7010b8730e24abe623f74f6ab4
Czakoi Levente <czakoilevente@gmail.com>
2019-03-11 16:26:11 +0100

    task done

--- w8/d1/click-three-times.js
+++ w8/d1/click-three-times.js
@@ -1,2 +1,23 @@
 'use strict';
 
+let button = document.querySelector('button');
+let pTag = document.createElement('p');
+let body = document.querySelector('body');
+let counter = 0;
+body.appendChild(pTag);
+
+window.onload = setTimeout(() => {
+  button.setAttribute('class', 'clickable')
+}, 5000);
+
+button.addEventListener('click', () => {
+  if (button.getAttribute('class') == 'clickable') {
+    counter += 1;
+    pTag.innerText = `Counter state: ${counter}`;
+    if (counter >= 3) {
+      let p2Tag = document.createElement('p');
+      body.appendChild(p2Tag);
+      p2Tag.innerText = '5 seconds elapsed and clicked 3 times';
+    };
+  };
+});

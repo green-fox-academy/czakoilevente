3a2c840bfd3f0fa57cb5108e1b0e03767451a189
Czakoi Levente <czakoilevente@gmail.com>
2019-03-21 15:11:01 +0100

    randomized questions, answers generated onload - all files modified accordingly

--- w9/quiz/assets/frontend.js
+++ w9/quiz/assets/frontend.js
@@ -2,12 +2,26 @@
 
 let http = new XMLHttpRequest;
 let body = document.querySelector('.body');
+let answerButtons = document.getElementsByClassName('button');
+let questionTag = document.querySelector('.question');
 
 window.onload = () => {
-  http.open('/game');
+  http.open('GET', '/game');
   http.onload = () => {
-    let data = JSON.parse(http.responseText);
+    let data = JSON.parse(http.response);
     console.log('dasd');
+    console.log(data);
+
+    questionTag.innerHTML = data[0].question;
+    for (let i = 0; i < answerButtons.length; i++) {
+      answerButtons[i].innerHTML = data[i].answer;
+      console.log(answerButtons[i].value);
+      console.log(data[i].value);
+    }
+
+    
+
 
   }
+  http.send();
 };
--- w9/quiz/server.js
+++ w9/quiz/server.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 const sql = require('mysql');
 
 app.listen(PORT, () => {
-  console.log(`Server is running, yay!  Port ${PORT}`);
+  console.log(`Server is running, yay! Port ${PORT}`);
 });
 
 const connectQuiz = sql.createConnection({
@@ -29,13 +29,37 @@ connectQuiz.connect((error) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', express.static('./assets'));
+app.use('/', express.static('assets'));
+
+app.get('/', (req, res) => {
+  console.log('/ get-method reqached the server, sending html to front...');
+  res.sendFile(path.join(__dirname, 'assets/index.html'));
+});
 
 app.get('/game', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  console.log('get request reached the server');
+
+  connectQuiz.query(`select question, answer, is_correct from answers left join questions on questions.id = answers.question_id where questions.id=${randomNumber(1,10)};`, (err, rows) => {
+    if (err) {
+      console.log('query failed');
+      res.status(501).send();
+    } else {
+      console.log('query ok, sending rows to frontend');
+
+      res.send(rows);
+    }
+  });
+  //res.sendFile(path.join(__dirname, 'assets/index.html'));
 });
 
 app.get('/questions', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index2.html'));
+  res.sendFile(path.join(__dirname, 'assets/index2.html'));
 });
 
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * max) + min;
+}
+
+function howManyQuestions () {
+  
+}

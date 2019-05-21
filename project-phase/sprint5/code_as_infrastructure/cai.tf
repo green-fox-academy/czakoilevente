0a2c2b439ae505aecab710d98c0d9fb7457972e4
CzakoiLevente <czakoilevente@gmail.com>
2019-05-21 15:15:52 +0200

    credentials modified as variables, added to ggitignore

--- project-phase/sprint5/code_as_infrastructure/cai.tf
+++ project-phase/sprint5/code_as_infrastructure/cai.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "xxx"
-  secret_key = "xxx"
-  region     = "us-east-1"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
 }
 
 resource "aws_instance" "example" {

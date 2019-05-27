40ac8c5d8c96d5d4dea255fb263c04e71a054279
CzakoiLevente <czakoilevente@gmail.com>
2019-05-27 10:51:53 +0200

    shell srcipt file added

---
+++ project-phase/sprint5/Securing_EC2_Computers/script.sh
@@ -0,0 +1,30 @@
+#update packages
+sudo apt-get update
+#Install packages to allow apt to use repository over https
+sudo apt-get install \
+apt-transport-https \
+ca-certificates \
+curl \
+software-properties-common
+#Add docker GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu | sudo apt-key add -
+#Add key fingerprint
+sudo apt-key fingerprint 0EBFCD88
+#Update index
+sudo apt-get update
+#install docker 
+sudo apt-get install docker-ce
+#install git
+sudo yum install -y git
+#docker as a non-root user
+sudo groupadd docker
+sudo usermod -aG docker $USER
+#create package.json file
+npm init -y
+#install node package manager
+npm install
+#install node and add dependency in json
+npm install node --save
+#install express and add dependency in json
+npm install express --save
+

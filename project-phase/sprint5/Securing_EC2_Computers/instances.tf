c01384a6d56572ec8b4a0021ca7d8d959ff0bbd9
CzakoiLevente <czakoilevente@gmail.com>
2019-05-27 11:07:31 +0200

    security group added

--- project-phase/sprint5/Securing_EC2_Computers/instances.tf
+++ project-phase/sprint5/Securing_EC2_Computers/instances.tf
@@ -8,6 +8,24 @@ provider "aws" {
   region     = "${var.region-aws}"
 }
 
+resource "aws_security_group" "ec2-allow-ssh" {
+  name = "ec2-allow-ssh"
+
+    ingress {
+    from_port = 22
+    to_port = 22
+    
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "securing" {
   ami = "ami-656be372"
   instance_type = "t1.micro"
